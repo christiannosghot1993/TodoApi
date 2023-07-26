6089fe48729df14c799c04506f597612585b6da3
Csuarez ice <csuarez.ice@ibm.com>
2023-07-26 08:57:28 -0500

    Preparacion del ambiente

---
+++ Models/MiDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+public class MiDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlite("Data Source=todo_db.db");
+    }
+
+    // Agrega tus DbSet y modelos aquí
+    public DbSet<ToDo> ToDo { get; set; }
+}
---
+++ Models/ToDo.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoApi.Models
+{
+    public class ToDo
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public bool Done { get; set; }
+    }
+}
